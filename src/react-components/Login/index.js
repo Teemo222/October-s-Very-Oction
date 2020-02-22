ec77da171d58a1c19f954b680986de20033b067c
chenhu84 <ht.chen@mail.utoronto.ca>
2020-02-22 01:58:15 -0500

    login works, so tired {{{(>_<)}}}

--- src/react-components/Login/index.js
+++ src/react-components/Login/index.js
@@ -4,21 +4,64 @@ import "./styles.css";
 import Header from '../Header';
 
 
-/* The SignUp Component */
+/* The Login Component */
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shown: false
+    };
+  }
+
   handleInputChange(e){
 
+  }
+
+  showLogin = () => {
+    const { shown } = this.state;
+    this.setState({
+      shown: true
+    });
+  }
+
+  cancelPopUp = (e) => {
+    const backgroundDiv = document.querySelector("#background-div");
+    if(e.target === backgroundDiv) {
+      this.setState({
+        shown: false
+      });
+    }
   }
 
   render() {  
     const {
       currentUser,
     } = this.props;
-    
+
+    const { showLogin, cancelPopUp } = this;
+    const { shown } = this.state;
+    let popup;
+    if(shown) {
+      popup = (
+        <div className="signup-popup-background" id="background-div" onClick={cancelPopUp}>
+          <div className="signup-popup" id="signup-div">
+          <form>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username"></input><br />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password"></input><br />
+            <button>Login</button>        
+          </form>
+        </div>
+        </div>
+      );
+    }
+
     return (  
-      <div>
-        <p>Login In</p>          
-      </div>  
+     <div>
+       <a onClick={showLogin}>Login</a>
+       {popup }
+     </div>
     );  
   }   
 }
