d7596b64ab4037801c93bf0cffe7d1a02d661982
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2020-02-25 14:34:38 -0500

    .

--- src/react-components/UserProfile/index.js
+++ src/react-components/UserProfile/index.js
@@ -4,20 +4,88 @@ import "./styles.css";
 import Header from '../Header';
 
 
+class Menu extends React.Component{
+  render() {
+    const {items } = this.props;
+    return (<div className="profile-menu">
+        {items}
+    </div>);
+  }
+}
+
+class MenuItem extends React.Component {
+  render(){
+    const { name, onClick } =this.props;
+    return (
+      <div className="profile-menu-item" onClick={onClick}>
+        <p>{name}</p>
+      </div>
+    )
+  }
+}
+
+class ProfileDetail extends React.Component {
+  render(){
+    return (<div>
+      Detail
+    </div>);
+  }
+}
+
+
+class PurchaseHistory extends React.Component {
+  render(){
+    return (<div>
+      PurchaseHistory
+    </div>);
+  }
+}
+
+class SellingHistory extends React.Component {
+  render(){
+    return (<div>
+      SellingHistory
+    </div>);
+  }
+}
+
 /* The SignUp Component */
 class UserProfile extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      activePage: ProfileDetail
+    }
+  }
+  setActive(page){
+    this.setState({activePage:page});
+  }
   render() {  
     const {
       currentUser,
     } = this.props;
+    let setActive = this.setActive.bind(this);
+    let items = [
+      (<MenuItem name="Profile" onClick={e=>{setActive(ProfileDetail)}}/>),
+      (<MenuItem name="Purchase History" onClick={e=>{setActive(PurchaseHistory)}}/>),
+      (<MenuItem name="Selling History" onClick={e=>{setActive(SellingHistory)}}/>),
+    ];
+    let menu = (<Menu items={items}></Menu>);
 
     return (  
-      <div className="userprofile__bg-image center">  
-         <Header currentUser = {currentUser}/>
-         <div>
-          <h1>Welcome, Mr.Nobody</h1>
-         </div>
-      </div>  
+      <div>
+         <div className="userprofile__bg-image center">  
+           <Header currentUser = {currentUser}/>
+           <br/>
+           <div className="profile-content">
+           {menu}
+           <div>
+             <this.state.activePage />
+           </div>
+          </div>          
+        </div>         
+      </div>
+     
     );  
   }   
 }
