bf88a7dcfae0b6a21e261601999ca5f88601f660
chenhu84 <ht.chen@mail.utoronto.ca>
2020-03-02 18:11:01 -0500

    user info table

--- src/react-components/UserTable/index.js
+++ src/react-components/UserTable/index.js
@@ -22,10 +22,7 @@ class UserTable extends React.Component {
           { title: 'Username', field: 'username' },
           { title: 'Password', field: 'password' },
         ],
-        data: [
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]
+        data: users
       }
     }
   
