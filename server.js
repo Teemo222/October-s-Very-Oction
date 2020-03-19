d6bdc22b8ca2cbc67d8e859070d9d7ee144f3084
chenhu84 <ht.chen@mail.utoronto.ca>
2020-03-19 18:00:20 -0400

    ??

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const port = process.env.PORT || 5000;
 
 const { mongoose } = require('./server/db/mongoose')
 
+const log = console.log
+
 // import the mongoose student
 const { Merchandise } = require('./server/models/Merchandise')
+const { User } = require('./server/models/User')
+const { Authenticator } = require('./server/models/Authenticator')
 
 // to validate object IDs
 const { ObjectID } = require('mongodb')
@@ -57,5 +61,53 @@ app.get('/items', (req, res) => {
 		res.send({ items }) // can wrap in object if want to add more properties
 	}, (error) => {
 		res.status(500).send(error) // server error
+	})
+})
+
+
+/* --------- User backend implementation    ------------------*/
+// (function() {
+// 	// code goes here
+// 	log("hi");
+// })();
+// ( () => {
+	// create admin
+	// try {
+	// 	let authenticator;
+	// 	authenticator = await Authenticator.findOne({
+	// 		username: "admin",
+	// 		password: "admin"
+	// 	});
+	// 	if(!authenticator) {
+	// 		authenticator = new Authenticator({
+	// 			username: "admin", 
+	// 			password: "admin"
+	// 		});
+	// 		authenticator = await authenticator.save();
+	// 	} 
+	// 	log("admin: ");
+	// 	log(authenticator);
+	// } catch(e) {
+	// 	log("create admin failed");
+	// 	log(e);
+	// }
+// }) ();
+
+
+// a POST route to *create* a user
+app.post('/users', (req, res) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	// Create a new student using the Student mongoose model
+	const { username, password } = req.body;
+	const user = new User({
+		username,
+		password
+	})
+
+	// Save student to the database
+	user.save().then((result) => {
+		res.send(result)
+	}, (error) => {
+		res.status(400).send(error) // 400 for bad request
 	})
 })
