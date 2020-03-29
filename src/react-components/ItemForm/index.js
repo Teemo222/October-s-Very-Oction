ec523f3e39d560d155217bc200ab9e989ac5a76d
chenhu84 <ht.chen@mail.utoronto.ca>
2020-03-29 17:50:07 -0400

    admin add image?

--- src/react-components/ItemForm/index.js
+++ src/react-components/ItemForm/index.js
@@ -12,18 +12,32 @@ class ItemForm extends React.Component {
   }
 
 	handleSubmit = async (e) => {
+    e.preventDefault();
     let formData = new FormData();
 
-    const name = document.querySelector("#name")
-		formData.append('name', name);
-    formData.append('image', this.state.file);
+    const itemName = document.querySelector("#name").value;
+    const itemCategory = document.querySelector("#category").value;
+    const itemDescription = document.querySelector("#description").value;
+		formData.append('itemName', itemName);
+		formData.append('itemCategory', itemCategory);
+		formData.append('itemDescription', itemDescription);
+    formData.append('itemImageSrc', this.state.file);
+
+    if(!itemName || !this.state.file || !itemCategory || !itemDescription) {
+      alert("please fill in all the fields");
+      return;
+    }
+
+    console.log("submit new item");
     
     const url = "http://localhost:5000/items"
     const response = await fetch(url, {
       method: 'POST',
       body: formData
     });  
-		
+    
+    const form = document.querySelector("#item-form");
+    form.reset();
 	}  
 
   handleChoose = (e) => {
@@ -36,17 +50,25 @@ class ItemForm extends React.Component {
     return (
 			<div>
         <h4>Add Item</h4>
-				<form onSubmit={this.handleSubmit}>
-        <fieldset>
+				<form id="item-form" onSubmit={this.handleSubmit}>
+          <fieldset>
 						<label htmlFor="name">Item Name: &nbsp;</label>
-						<input type="text"></input>
-					</fieldset>          
+						<input type="text" id="name"></input>
+					</fieldset>   
+          <fieldset>
+						<label htmlFor="category">Item Category: &nbsp;</label>
+						<input type="text" id="category"></input>
+					</fieldset>  
+          <fieldset>
+						<label htmlFor="description">Item Description: &nbsp;</label>
+						<input type="text" id="description"></input>
+					</fieldset>                             
 					<fieldset>
 						<label htmlFor="image">Choose avatar: &nbsp;</label>
-						<input type="file" onChange={this.handleChoose} name="image"></input>	
+						<input type="file" onChange={this.handleChoose} name="image" id="image"></input>	
 					</fieldset>
           <fieldset>
-            <button>Upload</button>
+            <button>Submit</button>
           </fieldset>
 				</form>				
 			</div>
