d1df79b699d1f6612fe13992a06229eec0af6431
zhaowe58 <xiaoteemo.zhao@.mail.utoronto.ca>
2020-04-02 17:36:53 -0400

    g

--- server.js
+++ server.js
@@ -32,10 +32,6 @@ const session = require('express-session')
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
 /*** Session handling **************************************/
 // Create a session cookie
 app.use(session({
